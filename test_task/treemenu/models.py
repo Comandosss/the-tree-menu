bfcba48c79a6d5e865914ea1bd1721890ad098e8
Comandosss <cotum@mail.ru>
2023-11-03 21:12:25 +0300

    added models

--- test_task/treemenu/models.py
+++ test_task/treemenu/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True,
+        blank=True, related_name='children'
+    )
+    menu = models.ForeignKey(
+        Menu, blank=True, related_name='items', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
